95046ff65a5d1e84c2609d4a38c1b70d7c257156
Kirill Dremin <missl.wipiece@gmail.com>
2025-09-25 13:06:09 +0300

    feat: added image pushing

---
+++ person-service/src/test/java/ru/uncledrema/personservice/domain/services/impl/PersonServiceImplTest.java
@@ -0,0 +1,28 @@
+package ru.uncledrema.personservice.domain.services.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonServiceImplTest {
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void find() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
