0539ef0653f0f4ef1a417d4d473ba660bb92e8fe
Kirill Dremin <missl.wipiece@gmail.com>
2025-09-25 16:08:01 +0300

    ci: restarting containers

--- ci/pull_and_update.sh
+++ ci/pull_and_update.sh
@@ -13,16 +13,14 @@ fi
 
 echo "Using compose file: $COMPOSE_FILE"
 
-# Pull updated images defined in compose file
+echo "Stopping services..."
+docker compose -f "$COMPOSE_FILE" rm -f
+
 echo "Pulling images..."
 docker compose -f "$COMPOSE_FILE" pull
 
-# Recreate containers with new images (no build)
 echo "Bringing services up..."
-docker compose -f "$COMPOSE_FILE" up -d --no-recreate
-
-# If you want to force recreate (stop/start) change to:
-# docker compose -f "$COMPOSE_FILE" up -d --force-recreate
+docker compose -f "$COMPOSE_FILE" up --build -d
 
 echo "Services status:"
 docker compose -f "$COMPOSE_FILE" ps
